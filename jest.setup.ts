9a93faae7387b51086cd3eb8f9664153b771c859
Brent Honeybone <brent@glacial.nz>
2021-01-06 18:59:04 +0100

    chore: add jest boot for test env

---
+++ jest.setup.ts
@@ -0,0 +1,3 @@
+export default (): void => {
+  process.env.BASE_DIRECTORY = `${__dirname}/src`
+}
---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+describe('index', () => {
+  it ('is truthy', () => {
+    expect(true).toBeTruthy()
+  })
+})
